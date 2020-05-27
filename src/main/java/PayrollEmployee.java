b994d3e3c96cbc016a89aeaadd11dcad0d6ec514
Rchambe9 <Rchambe9@gmail.com>
2020-05-27 15:03:50 -0400

    First Draft

---
+++ src/main/java/PayrollEmployee.java
@@ -0,0 +1,27 @@
+public class PayrollEmployeeID {
+
+    private String firstName, lastName;
+    private double ratePerHour;
+    private int employeeId;
+    private double payCheckTotal;
+
+
+    public PayrollEmployeeID(String firstName, String lastName, double ratePerHour, int employeeId, double payCheckTotal) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ratePerHour = ratePerHour;
+        this.employeeId = employeeId;
+        this.payCheckTotal = payCheckTotal;
+
+        if {
+    }
+
+
+
+
+
+}
+
+    public void setPayCheckTotal(double payCheckTotal) {
+        this.payCheckTotal = payCheckTotal;
+    }
